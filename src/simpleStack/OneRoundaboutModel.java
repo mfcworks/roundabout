8aed00e2577f72e2d55360903af8382e5f30d277
Tomoki <lilac.tm.0x1728@live.jp>
2015-11-17 09:49:00 +0900

    Create simpleStack package by forking roundabout

---
+++ src/simpleStack/OneRoundaboutModel.java
@@ -0,0 +1,192 @@
+package simpleStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * １交差点モデル（L = 1, m = 比較的長い）
+ * @author T. Miyazaki
+ *
+ * 以前の版は一時的に以下に退避
+ * https://gist.github.com/t-miyazaki/381d6af5a7819160988d
+ *
+ */
+public class OneRoundaboutModel {
+
+	private int m;			// 道路リンクの長さ
+	private Cell cell;		// 単位セル
+	private List<Car> cars;	// 車のデータ
+
+
+	/**
+	 * コンストラクタ：モデルを構築します。
+	 *
+	 * @param L 正方格子の一辺の数
+	 * @param m 道路サイトの長さ
+	 */
+	public OneRoundaboutModel(int m) {
+		this.m = m;
+
+		cell = new Cell(0, 0, m);
+
+		// セルの接続を設定する。今回は1つのセルで周期境界にする。
+		cell.setNeighbors(cell, cell, cell, cell);
+
+
+		/*
+			// ★セル同士の結合の設定
+			// システムを上空から見た場合、左上(top-left)を[0][0]、
+			// 右下(right-bottom)を[L-1][L-1]の座標になるようにする。
+			// インデックス[i][j]は、[左右方向][上下方向]の順で用いる。
+			// なお、原論文では、上下方向の軸は上に行くほど小となっている。
+			// 本実装では、利便性のため、これを逆転させているが、
+			// 表記が変わるだけで本質的には何の違いもない。
+			for (int i = 0; i < L; i++) {
+				for (int j = 0; j < L; j++) {
+					// [i][j]のleftは[i-1][j]
+					int left   = i==0 ? L-1 : i-1;
+					// [i][j]のtopは[i][j-1]
+					int top    = j==0 ? L-1 : j-1;
+					// [i][j]のrightは[i+1][j]
+					int right  = i==L-1 ? 0 : i+1;
+					// [i][j]のbottomは[i][j+1]
+					int bottom = j==L-1 ? 0 : j+1;
+
+					cells[i][j].setNeighbor(
+							cells[left][j],
+							cells[i][top],
+							cells[right][j],
+							cells[i][bottom]);
+				}
+			}
+		 */
+
+	}
+
+	/**
+	 * 初期化：初期状態を生成します。
+	 *
+	 * @param numCars 発生させる車の台数
+	 */
+	public void initialize(int numCars) {
+
+		// 1セルの全サイト数 = 4 * (m + 1)
+		// 車の数はそれより少ない必要がある。
+		// ※ 正方格子系の場合は i * j * 4 * (m + 1) にする。
+		assert (numCars <= 4 * (m + 1));
+
+		cars = new ArrayList<Car>(numCars);
+
+		// dummy car (empty)
+		cars.add(0, new Car(0));
+
+		// CellとCarでの相互参照ができるようにする
+		Cell.carList = cars;
+		Car.cell = cell;
+		Car.m = this.m;
+
+		// numCars 台の車を発生
+		for (int i = 1; i <= numCars; i++) {
+			cars.add(i, new Car(i));
+		}
+	}
+
+
+	// アップデート
+	public void update() {
+		// (全ての)セルをupdateしてから
+		cell.updateCell();
+		// (全ての)セルのバッファをスワップする
+		cell.swapBuffer();
+	}
+
+	// 境界条件の処理
+	public void spwanAndDespawn() {
+		// 周期境界でずっと回り続けるので、
+		// とりあえずは実装しなくて良い。
+	}
+
+
+	private String mk(int val) {
+		return (val == 1 ? "*" : "･");
+	}
+
+	// mが偶数のときのみ使える簡易テキストプロッタ
+	public void textPlot() {
+		if (m % 2 != 0) return;
+
+		int half = m / 2;
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < half; i++)
+			sb.append(" ");
+
+		String blank = sb.toString();
+
+		// 上に出た縦の道路
+		for (int i = 0; i < half; i++) {
+			System.out.print(blank);
+			System.out.print(mk(cell.mu[1][half-i]));
+			System.out.println(mk(cell.mu[3][half+i+1]));
+		}
+
+		// 横の道路の上側
+		for (int i = 0; i < half; i++)
+			System.out.print(mk(cell.mu[2][half+1+i]));
+		System.out.print(mk(cell.mu[1][0]));
+		for (int i = 0; i < half + 1; i++)
+			System.out.print(mk(cell.mu[2][i]));
+		System.out.println();
+
+		// 横の道路の下側
+		for (int i = half; i >= 0; i--)
+			System.out.print(mk(cell.mu[0][i]));
+		System.out.print(mk(cell.mu[3][0]));
+		for (int i = m; i > half; i--)
+			System.out.print(mk(cell.mu[0][i]));
+		System.out.println();
+
+		// 下に出た縦の道路
+		for (int i = 0; i < half; i++) {
+			System.out.print(blank);
+			System.out.print(mk(cell.mu[1][m-i]));
+			System.out.println(mk(cell.mu[3][i+1]));
+		}
+
+		System.out.println();
+	}
+
+
+
+	// ----- test -----
+	public static void main(String[] args) {
+		int my_m = 20;
+		int my_n = 40;
+
+		Scanner sc = new Scanner(System.in);
+
+		OneRoundaboutView view = new OneRoundaboutView(600, 600);
+
+		// モデルを作る
+		OneRoundaboutModel model = new OneRoundaboutModel(my_m);
+
+		// 初期状態の生成
+		model.initialize(my_n);
+
+		// 無限ループ
+		for (;;) {
+			view.drawCell(model.cell);
+			model.update();
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+				break;
+			}
+		}
+
+		System.out.println("終了");
+	}
+
+}
---
+++ src/simpleStack/OneRoundaboutView.java
@@ -0,0 +1,151 @@
+package simpleStack;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class OneRoundaboutView extends JPanel {
+
+	private JFrame frame;
+	private Cell cell;
+
+	/**
+	 * ウィンドウを作成します。
+	 *
+	 * @param winX 横幅のピクセル数
+	 * @param winY 縦幅のピクセル数
+	 */
+	public OneRoundaboutView(int winX, int winY) {
+
+		/*
+		 * メインウィンドウ
+		 */
+		// 適当なタイトルでウィンドウを作成
+		frame = new JFrame("OneRoundaboutModel Viewer");
+		// 閉じるボタンで終了する
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// OSがデフォルトで提供する位置にウィンドウを表示する
+		frame.setLocationByPlatform(true);
+		// サイズ変更不可能にする
+		frame.setResizable(false);
+		// サイズを設定
+		frame.setSize(winX, winY);
+
+		/*
+		 * 描画用パネル
+		 */
+//		this.setBackground(bg);
+		// このJPanelを追加
+		frame.add(this); // frame.getContentPane().add(this); でなくても良いみたい
+
+		// メインウィンドウを表示する
+		frame.setVisible(true);
+	}
+
+
+	/**
+	 * 現在のCell(配列)の状態を描画します。
+	 *
+	 * @param cell
+	 */
+	public void drawCell(Cell cell) {
+		this.cell = cell;
+
+		this.repaint();
+	}
+
+	// this.repaint() から呼び出される
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		if (cell != null) drawImpl(g);
+	}
+
+	// 中心(x, y), 半径rの閉じた円を描画
+	private static void circle(Graphics g, int x, int y, int r) {
+		g.fillOval(x - r, y - r, 2 * r, 2 * r);
+	}
+
+	// 実際に描画する処理
+	private void drawImpl(Graphics g) {
+		// 背景を 白色 で塗りつぶす
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		g.setColor(Color.BLUE);
+
+
+		int offsetX = 10, offsetY = 10; // 描画起点位置
+		int unit = 20; // 単位要素のサイズ
+
+		int m = cell.m;
+		assert m % 2 == 0; // mは偶数を仮定
+
+		int mh = m / 2; // m half
+
+		/*
+		 * 枠線を描画
+		 */
+		g.setColor(Color.BLACK);
+		for (int i = 0; i < 3; i++) {
+			// 縦方向の車線の縦の長い線
+			int x = offsetX + unit * (mh + i);
+			int y = offsetY + unit * (m + 2);
+			g.drawLine(x, offsetY, x, y);
+			// 横方向の車線の横の長い線
+			x = offsetX + unit * (m + 2);
+			y = offsetY + unit * (mh + i);
+			g.drawLine(offsetX, y, x, y);
+		}
+		for (int i = 0; i < m + 3; i++) {
+			// 縦方向の車線の横の短い線
+			int x = offsetX + unit * mh;
+			int y = offsetY + unit * i;
+			g.drawLine(x, y, x + unit * 2, y);
+			// 横方向の車線の縦の短い線
+			x = offsetX + unit * i;
+			y = offsetY + unit * mh;
+			g.drawLine(x, y, x, y + unit * 2);
+		}
+
+		/*
+		 * 車を描画
+		 */
+		String[] arrows = {"←", "↑", "→", "↓"};
+		int[] x = {offsetX + unit * mh, offsetX + unit * mh,
+				offsetX + unit * (mh + 1), offsetX + unit * (mh + 1)};
+		int[] y = {offsetY + unit * (mh + 1), offsetY + unit * mh,
+				offsetY + unit * mh, offsetY + unit * (mh + 1)};
+		for (int i = 0; i < m + 1; i++) {
+			for (int a = 0; a < 4; a++) {
+				if (cell.mu[a][i] == 1) {
+					int e = 2; // サイズ調整の縁
+					g.setColor(Color.CYAN);
+					g.fillOval(x[a] + e, y[a] + e, unit - 2*e, unit - 2*e);
+					g.setColor(Color.BLACK);
+					String snum = String.valueOf(cell.num[a][i]);
+					g.drawString((snum.length() == 1 ? " " + snum : snum), x[a]+3, y[a]+15);
+				} else if (i != 0) {
+					g.setColor(Color.BLACK);
+					g.drawString(arrows[a], x[a]+5, y[a]+15);
+				}
+				switch (a) {
+				case 0: x[0] -= unit; break;
+				case 1: y[1] -= unit; break;
+				case 2: x[2] += unit; break;
+				case 3: y[3] += unit; break;
+				}
+			}
+
+			if (i != mh) continue;
+			//切り替え
+			x[0] = offsetX + unit * (m + 1);
+			y[1] = offsetY + unit * (m + 1);
+			x[2] = offsetX;
+			y[3] = offsetY;
+		}
+	}
+}
